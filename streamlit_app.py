000925cbb015c10eff96c585be788ae37e6d61de
tachi-kei <160448699+tachi-kei@users.noreply.github.com>
2024-02-19 16:18:05 +0900

    Update streamlit_app.py
    例を表示

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # スムージーにいれたいフルーツを選択
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 # ページ内に収まるサイズでフルーツリストを表示
 streamlit.dataframe(my_fruit_list)
