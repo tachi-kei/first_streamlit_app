d8b3c94534e9dff854b75e745adaad57cab66a3d
tachi-kei <160448699+tachi-kei@users.noreply.github.com>
2024-02-19 16:12:33 +0900

    Update streamlit_app.py
    フルーツを選択できる機能追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# スムージーにいれたいフルーツを選択
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# ページ内に収まるサイズでフルーツリストを表示
 streamlit.dataframe(my_fruit_list)
