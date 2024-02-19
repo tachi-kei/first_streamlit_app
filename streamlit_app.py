3864a75ac94bd3e3d9acd0f0e103da3ff4c4634a
tachi-kei <160448699+tachi-kei@users.noreply.github.com>
2024-02-19 16:15:00 +0900

    Update streamlit_app.py
    名前で選択できるように機能追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # スムージーにいれたいフルーツを選択
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
