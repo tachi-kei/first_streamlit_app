ca64ba60f3864b9afadd0a8eb9dd90058612672d
tachi-kei <160448699+tachi-kei@users.noreply.github.com>
2024-02-21 21:26:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 # リストからフルーツを加えられるようにテキストボックスを作成
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.text('Thanks for adding' + add_my_fruit)
+streamlit.write('Thanks for adding', add_my_fruit)
