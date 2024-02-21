67ada4cc84ec62a658b8d3f6f30a306ac0fe61bf
tachi-kei <160448699+tachi-kei@users.noreply.github.com>
2024-02-21 22:38:04 +0900

    Update streamlit_app.py
    インデントの調整

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
+# APIのインポート
 import streamlit
 import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
+# タイトルを表示
 streamlit.title('My Parents New Healthy Diner')
-
+# ヘッダーを表示
 streamlit.header('Breakfast Menu')
+# メニューをテキストで表示
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
@@ -14,28 +17,34 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
+# スムージーに入れたいフルーツを選択する
+# AWSのS3に接続し、フルーツのデータを取得
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# スムージーにいれたいフルーツを選択
+# フルーツを選択できるようにする
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# ページ内に収まるサイズでフルーツリストを表示
+# 選択したフルーツ一覧を表示
 streamlit.dataframe(fruits_to_show)
 
+
+# 政府のデータからフルーツの情報を参考にできるようにする
 # get_fruityvice_data関数を定義
+# リクエストパラメーターを送信
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
   
-# api responseを表示
+# 一覧のタイトル表示
 streamlit.header("Fruityvice Fruit Advice!")
 try:
+  # テキストボックスに入力された情報を変数に代入
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  # テキストボックスが空の場合エラーメッセージを表示
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
+  # テキストボックスに情報が入っている場合一覧を表示する
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
@@ -43,7 +52,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-# don't run anything past here while we troubleshoot
+# 以下の処理を無効にする
 streamlit.stop()
 
 # snowflakeからデータを表示
