085670263edde7ee08205dc9a6dfde5ee03c358d
tachi-kei <160448699+tachi-kei@users.noreply.github.com>
2024-02-21 14:39:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # streamlit.text(fruityvice_response.json())　json表示を削除
 
 # json形式を普通の形式に変換する
